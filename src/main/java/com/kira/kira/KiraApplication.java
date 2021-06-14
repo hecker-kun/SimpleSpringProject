4012701dab3fea802092cac59bef4a19ad073a2e
Alexander Pikeev <alexanderpikeev@gmail.com>
2021-06-14 18:46:55 +0300

    Init

---
+++ src/main/java/com/kira/kira/KiraApplication.java
@@ -0,0 +1,12 @@
+package com.kira.kira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiraApplication.class, args);
+    }
+}
